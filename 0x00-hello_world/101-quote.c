a12cb5191e27f921dbf413813791247cc3e0406d
samakintayo <tayoakinnayajo@gmailcom>
2022-10-14 03:54:32 +0000

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+ * main - Entry poin
+ * Return: Always 1 (Sucess)
+*/
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+			\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
