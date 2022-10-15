ca9e034f4c8f972a97e5e7d53f4eb4ab9fbefc64
samakintayo <tayoakinnayajo@gmailcom>
2022-10-15 04:35:11 +0000

    fixed betty error

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-	if (num == 9)
-		continue;
+	for (num = 0; num < 10; num++){
+		putchar((num % 10) + '0')
+		if (num == 9)
+			continue;
 	putchar (',');
 	putchar (' ');
-
+	}
 	putchar('\n');
 
 	return (0);
