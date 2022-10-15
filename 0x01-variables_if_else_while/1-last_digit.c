4b0d02718b253537ad581ac3780b2e9222adf7ea
samakintayo <tayoakinnayajo@gmailcom>
2022-10-15 04:58:48 +0000

    hot fix betty erro

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number and
- * whether it is greater than 5, less than 6, or 0.
- *
+ * main - This program will assign a random number to the variable n each time
+ * it is executed. Complete the source code in order to print the last
+ * digit of the number stored in the variable n *
  * Return Always 0.
  */
 int main(void)
