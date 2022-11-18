8546e073b6354d8eeac68d4e17ac669e6eb708e2
samakintayo <tayoakinnayajo@gmailcom>
2022-11-18 21:40:01 +0000

    more fix

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,10 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct op - Struct op
  * @op: The operator
