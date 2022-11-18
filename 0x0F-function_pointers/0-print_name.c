d5701ca91ce1a99a89c3d1dbe7fe1fba160c5156
samakintayo <tayoakinnayajo@gmailcom>
2022-11-18 21:37:16 +0000

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -8,6 +9,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (!name || !f)
+		return;
+
 	f(name);
 }
